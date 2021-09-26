a4314224dd32c0068a2e527d90a5beb812844f92
snehak2299 <skanase2299@gmail.com>
2021-09-27 00:09:39 +0530

    [sneha]: give greeting msg with firstname + lastname or simply hello world

--- src/main/java/com/bridglab/greeting/controller/GreetingController.java
+++ src/main/java/com/bridglab/greeting/controller/GreetingController.java
@@ -2,6 +2,8 @@ package com.bridglab.greeting.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +14,20 @@ public class GreetingController {
 	private static final String template="Hello %s";
 	private final AtomicLong counter= new AtomicLong();
 	
-	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam (value="name",defaultValue="world") String name) {
+	@GetMapping("/greeting")        // UC1 return json
+	public Greeting greeting(@RequestParam (value="name",defaultValue="Hello world") String name) {
 		
 		return new Greeting(counter.incrementAndGet(),String.format(template, name));
 		
 	}
-	@GetMapping("/start")
+	@GetMapping("/start")  // UC2  hello world
 	public String home() {
 		return "Hello world !!";
+	}
+	
+	@RequestMapping(value= {"/query"},method=RequestMethod.GET)  //UC3 firstname + lastname acc to user
+	public String SayHello(@RequestParam (value="name",defaultValue="Hello world") String name,@RequestParam(value="firstName") String firstName,@RequestParam(value="lastName")String lastName) {
+		return new Greeting(counter.incrementAndGet(),String.format(template, name))+firstName+" "+lastName+"";
+	
 	}
 }
